a51180b37343dc8d58c99492775bf4a4d443da6b
bhuvanameenakshikoteeswaran <bhuvanameenakshi@gmail.com>
2021-11-06 17:41:11 +0530

    fixed error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,10 +28,10 @@
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
+<!--
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
+                        @endif-->
                     @endauth
                 </div>
             @endif
